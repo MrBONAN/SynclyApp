f99dfc10a3dfd17ddc66305075791264dfc324cc
Fridmak <apbatin2005@gmail.com>
2024-11-25 23:21:50 +0500

    TryMapDo

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -11,15 +11,8 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnMapOpened(object sender, EventArgs e)
     {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Fuck it {count} time";
-        else
-            CounterBtn.Text = $"Fuck it {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        await Navigation.PushAsync(new MapPage());
     }
 }
---
+++ App/MapPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure;
+
+namespace App;
+
+public partial class MapPage : ContentPage
+{
+    int count = 0;
+
+    public MapPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        var assembly1 = typeof(MapPage).Assembly;
+        string[] resourceNames = assembly1.GetManifestResourceNames();
+
+        foreach (var resourceName in resourceNames)
+        {
+            Console.WriteLine(resourceName);
+        }
+
+        var assembly = typeof(MapPage).Assembly;
+        string resourcePath = "App.Resources.Raw.Map.html";
+
+        using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            string htmlContent = reader.ReadToEnd();
+            LeafletWebView.Source = new HtmlWebViewSource
+            {
+                Html = htmlContent
+            };
+        }
+    }
+}
