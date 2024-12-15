b7cf977c56fe7f651fcf28f3a8d93af0137abced
Mot1x <motixflawless@gmail.com>
2024-12-15 23:19:08 +0500

    добавил свойства

--- Domain/Artist.cs
+++ Domain/Artist.cs
@@ -2,14 +2,15 @@ namespace Domain;
 
 public class Artist
 {
-    public readonly string? Id;
-    public readonly string? Name;
-    public readonly List<string>? Genres;
-    public readonly string? ProfileImageURL;
-    public readonly Dictionary<MusicServices, string?> Links = new()
+    public string? Id { get; private set; }
+    public string? Name { get; private set; }
+    public List<string>? Genres { get; private set; }
+    public string? ProfileImageURL { get; private set; }
+
+    public Dictionary<MusicServices, string?> Links { get; private set; } = new()
     {
-        {MusicServices.Spotify, null},
-        {MusicServices.YandexMusic, null}
+        { MusicServices.Spotify, null },
+        { MusicServices.YandexMusic, null }
     };
 
     public Artist(string? id, string? profileImageURL, string? name)
@@ -18,7 +19,7 @@ public class Artist
         Name = name;
         ProfileImageURL = profileImageURL;
     }
-    
+
     public Artist(Infrastructure.API.SpotifyAPI.Models.Artist artist)
     {
         Id = artist.Id;
@@ -27,6 +28,6 @@ public class Artist
         Links[MusicServices.Spotify] = artist.Uri;
         Genres = artist.Genres;
     }
-    
+
     public string ToString() => Name;
 }
